aa2c9de5fda3f36aefd6f0cc31b134a76284d4a6
Harper <harper@barnett.work>
2020-10-19 20:34:50 +0100

    Inital commit... 2!

---
+++ discordbot.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json')
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+    if(!(message.content.toLowerCase().includes('pog')||message.author.bot)){
+        message.reply(" that wasn't very pog of you.")
+        message.delete();
+    }
+})
+
+client.on('messageUpdate', (oldMessage, message) => {
+    if(!(message.content.toLowerCase().includes('pog')||message.author.bot)){
+        message.reply(" that wasn't very pog of you.")
+        message.delete();
+    }
+})
+
+client.login(config.token);
